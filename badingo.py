66c4003dd49133231b17a4466ed8b9c9c3b5a561
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-03-15 16:17:29 -0400

    Extract tring from HTML

--- badingo.py
+++ badingo.py
@@ -1,17 +1,26 @@
 # Building my first scraper!
-
-# 1. Extract HTML
-
 from urllib.request import urlopen
 
+
+# 1. Extract HTML
 url = "https://badingo.net"
 page = urlopen(url)
 print(page)
-
 
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
 print(html)
 
-# 2. Extract string from HTML
 
+# 2. Extract string from HTML
+title_index = html.find("<title>")
+print(title_index)
+
+start_index = title_index + len("<title>")
+print(start_index)
+
+end_index = html.find("</title>")
+print(end_index)
+
+title = html[start_index:end_index]
+print(title)
