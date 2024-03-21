506b4cf890ef2ca20ccc6b462028af606a38d7a8
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-03-21 17:35:00 -0400

    Success! Selenium scraper now closes popup and loads all units

--- apartment_prices_selenium.py
+++ apartment_prices_selenium.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -11,13 +14,30 @@ load_dotenv()
 options = webdriver.ChromeOptions()
 options.headless = True
 driver = webdriver.Chrome(
-    service=ChromeService(ChromeDriverManager().install, options=options())
+    service=ChromeService(ChromeDriverManager().install()), options=options
 )
 
-url = os.getenv("APARTMENT_URL")
 
+url = os.getenv("APARTMENT_URL")
 driver.get(url)
+
+
+wait = WebDriverWait(driver, 10)
+wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "ant-modal-close")))
+close_button = driver.find_element(By.CLASS_NAME, "ant-modal-close")
+close_button.click()
+
+
+load_all_button = driver.find_element(By.ID, "load-all-units")
+wait = WebDriverWait(driver, 10)
+wait.until(EC.element_to_be_clickable((By.ID, "load-all-units")))
+driver.execute_script("arguments[0].scrollIntoView();", load_all_button)
+actions = ActionChains(driver)
+actions.move_to_element(load_all_button).click().perform()
+
 apt_elements = driver.find_elements(By.CLASS_NAME, "unit-item-details")
 for apt_element in apt_elements:
-    available_date = apt_element.find_element("div", class_="available-date")
+    available_date = apt_element.find_element(By.CLASS_NAME, "available-date")
     print(available_date.text.strip())
+
+driver.quit()
